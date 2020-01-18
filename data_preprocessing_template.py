7ae9e44db6d627f35b1ad88c692f47c2140e13cf
Yathsih <48596315+Yathsih@users.noreply.github.com>
2020-01-18 20:07:40 +0530

    Add files via upload

--- data_preprocessing_template.py
+++ data_preprocessing_template.py
@@ -6,13 +6,13 @@ import matplotlib.pyplot as plt
 import pandas as pd
 
 # Importing the dataset
-dataset = pd.read_csv('Salary_Data.csv')
+dataset = pd.read_csv('Data.csv')
 X = dataset.iloc[:, :-1].values
-y = dataset.iloc[:, 1].values
+y = dataset.iloc[:, 3].values
 
 # Splitting the dataset into the Training set and Test set
-from sklearn.model_selection import train_test_split
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size =1/3 , random_state = 0)
+from sklearn.cross_validation import train_test_split
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.2, random_state = 0)
 
 # Feature Scaling
 """from sklearn.preprocessing import StandardScaler
@@ -21,21 +21,3 @@ X_train = sc_X.fit_transform(X_train)
 X_test = sc_X.transform(X_test)
 sc_y = StandardScaler()
 y_train = sc_y.fit_transform(y_train)"""
-from sklearn.linear_model import LinearRegression
-l=LinearRegression()
-l.fit(X_train,y_train)
-y_pred=l.predict(X_test)
-plt.scatter(X_train,y_train,color='Red')
-plt.plot(X_train,l.predict(X_train),color ='Blue')
-plt.title('Salary vs Experience (Training set)')
-plt.xlabel('Years of Experience')
-plt.ylabel('Salary')
-plt.show()
-
-# Visualising the Test set results
-plt.scatter(X_test, y_test, color = 'red')
-plt.plot(X_train, l.predict(X_train), color = 'blue')
-plt.title('Salary vs Experience (Test set)')
-plt.xlabel('Years of Experience')
-plt.ylabel('Salary')
-plt.show()
